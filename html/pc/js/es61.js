aa8d3c86c854a293d0b18867441c6e0213f8f166
lh <1039495585@qq.com>
2022-04-12 09:49:37 +0800

    debug

--- html/pc/js/es61.js
+++ html/pc/js/es61.js
@@ -2,7 +2,7 @@
 /*
 几种模块化规范----语法不一样！！（https://www.jianshu.com/p/8f6770f1f751）：
 =》ES6  JS语法（高版本浏览器直接支持可以跑）
-=》CommonJs    node.js语法   （微信小程序使用的是commonjs的规范）：
+=》CommonJs    node.js语法就是CommonJs规范   （微信小程序支持commonjs规范，同时还支持官方的ES6规范）：
 module.exports={
   name:"我是抛出的模块"
 }
