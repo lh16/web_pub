06b530e736eff8e6a1fe0f01c3a6a57214910af5
lh <1039495585@qq.com>
2022-04-13 17:43:45 +0800

    debug

--- html/vuedemo/my-project/src/main.js
+++ html/vuedemo/my-project/src/main.js
@@ -7,4 +7,5 @@ createApp(App).use(router).mount('#app')
 /*const app = createApp(App)
 app.use(router)
 app.mount('#app')*/
+//打包：使用工具 vue-cil  或 webpack，配置打包文件在项目目录下npm run build 就打包了。（https://www.jb51.net/article/233569.htm）
 
