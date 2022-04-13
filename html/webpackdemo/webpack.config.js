d85859105fec7838184433f295da7adb73c7957a
lh <1039495585@qq.com>
2022-04-13 09:28:50 +0800

    debug

--- html/webpackdemo/webpack.config.js
+++ html/webpackdemo/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack'); 
-module.exports = { 
+module.exports = {
+	mode : 'production',//这项不配置的话运打包命令会报警告信息："WARNING in configuration The 'mode' option has not been set, ..."//development为开发者环境，production为生产环境变量 ，还有一个为none
     entry:{
         app:__dirname+'/src/app.js',//唯一入口文件,__dirname是nodejs里的一个全局变量，它指向的是我们项目的根目录
     },
@@ -12,7 +13,7 @@ module.exports = {
 //教程：https://cloud.tencent.com/developer/article/1195063
 //步骤总结： 安装nodejs；安装webpack;配置webpack.json；配置webpack.conf.js；命令行运行webpack命令；html引入打包后的js文件。
 //注意，命令行直接执行webpack，前提是全局安装了webpack，如果不是全局安装，还需要在后面加上入口文件的路径。
-//package.json文件：
+//package.json文件scripts标签下：
 //"start":"webpack"     npm start 
 //"dev":"webpack"     npm run dev 
 //"dev":"webpack --mode development"     npm run dev   开发模式（与生产模式打包压缩后的代码不一样，体积较大）
