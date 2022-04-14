3446869d56426fb1438bac2a3bf8f0f7671cdb76
lh <1039495585@qq.com>
2022-04-14 15:04:58 +0800

    1

--- html/vuedemo/my-project/vue.config.js
+++ html/vuedemo/my-project/vue.config.js
@@ -2,3 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+//Webpack相关配置(https://www.jianshu.com/p/b358a91bdf2d) : configureWebpack chainWebpack
+//vue-cli是vue官方出品的快速构建单页应用的脚手架，里面集成了webpack，npm，nodejs，babel，vue，vue-router
